214812b733a8f67251add9c9a75379425ab40a95
Xcaret García <xcaret.gc4@gmail.com>
2025-11-26 19:16:45 -0600

    Primer commit

---
+++ src/sistematutoriasfx/modeloo/ConexionBD.java
@@ -0,0 +1,14 @@
+package sistematutoriasfx.modeloo;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author JOANA XCARET
+ */
+public class ConexionBD {
+    
+}
